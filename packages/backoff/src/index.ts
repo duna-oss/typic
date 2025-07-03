6d1081c5bd35536637a694b9f12f036effa0c7ee
Frank de Jonge <info@frankdejonge.nl>
2025-07-03 15:19:53 +0200

    Make backoff depend on standard-error

--- packages/backoff/src/index.ts
+++ packages/backoff/src/index.ts
@@ -1,19 +1,11 @@
+import {StandardError} from '@typic/error-standard';
+
 export interface BackOffStrategy {
     // Returns the delay to use for the next try, or undefined if max tries has been exceeded
     backOff(attempt: number): number;
 }
 
-export class MaxAttemptsExceeded extends Error {
-    constructor(
-        message: string,
-        public readonly code: string,
-        public readonly context: {attempt: number},
-        cause: unknown = undefined,
-    ) {
-        const options = cause === undefined ? undefined : {cause};
-        super(message, options);
-    }
-
+export class MaxAttemptsExceeded extends StandardError {
     static atAttempt = (attempt: number) => new MaxAttemptsExceeded(
         'Max attempts exceeded',
         'backoff_strategy.error.max_attempts_exceeded',
--- packages/error-standard/src/index.ts
+++ packages/error-standard/src/index.ts
@@ -1,6 +1,5 @@
 export type ErrorContext = { [index: string]: string | number | null };
 
-// if we can find a way for this not to be an abstract class, that'd be great
 export abstract class StandardError extends Error {
     constructor(
         message: string,
@@ -9,8 +8,6 @@ export abstract class StandardError extends Error {
         cause: unknown = undefined,
     ) {
         const options = cause === undefined ? undefined : {cause};
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore TS2554
         super(message, options);
     }
 }
